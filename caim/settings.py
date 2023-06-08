53b0e1beb985c842ecd3f80d03f6f0cdc2d6fc4b
Raymond Gasper <14928724+rgasper@users.noreply.github.com>
2023-06-07 21:14:13 -0400

    add hot reloader for easier development

--- caim/settings.py
+++ caim/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     "avatar",
     "storages",
     "caim_base",
+    "django_browser_reload",
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "caim.urls"
